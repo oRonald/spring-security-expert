c74d733dfdb22167a37db066ff428c8f3d55b215
oRonold <or040918@gmail.com>
2025-10-20 14:38:57 -0300

    first commit

---
+++ src/main/java/br/com/expert_security/ExpertSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.expert_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertSecurityApplication.class, args);
+	}
+
+}
